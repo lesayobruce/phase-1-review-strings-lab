b988a52ead85e1a292b36547c713af0ef860982e
mashughuli <lesayobruce419@gmail.com>
2022-11-25 15:45:47 -0500

    completed phase-1-review-string-lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = "BRUCE LESAYO"
+
+
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage= `Welcome To Flatbook, ${currentUser}!`
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser .slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
+
+
+
+
+
